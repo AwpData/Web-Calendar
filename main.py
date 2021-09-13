104516e66d4fa7944f427c4f539b40096bdd9978
Trevor Tang <awpdata@gmail.com>
2021-09-13 16:22:42 -0700

    Final project pushed; Added specific searching, deleting, and range finding

--- main.py
+++ main.py
@@ -1,25 +1,20 @@
 import sys
 from datetime import datetime
 
-from flask import Flask
+from flask import Flask, abort, request
 from flask_restful import Api, Resource, reqparse, inputs
 from flask_sqlalchemy import SQLAlchemy
 
+# Configuring application and setting up Flask-SQLalchemy
 app = Flask(__name__)
-
-db = SQLAlchemy(app)
-api = Api(app)
-parser = reqparse.RequestParser()
-
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///name.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-parser.add_argument(
-    "message",
-    type=str,
-    help="",
-    required=False
-)
+db = SQLAlchemy(app)  # Creating DB
+db.create_all()
+api = Api(app)  # Creating API for HTTP
+parser = reqparse.RequestParser()  # Creating argument parser for proper GET requests
+
 parser.add_argument(
     "event",
     type=str,
@@ -34,7 +29,7 @@ parser.add_argument(
 )
 
 
-class Calendar(db.Model):
+class Calendar(db.Model):  # This is the DB table Calendar
     id = db.Column(db.Integer, primary_key=True)
     event = db.Column(db.String, nullable=False)
     date = db.Column(db.Date, nullable=False)
@@ -42,47 +37,58 @@ class Calendar(db.Model):
 
 class apiResource(Resource):  # This is at .../event 
     def get(self):
-        dates = Calendar.query.all()
+        if len(request.args) > 0:  # IF there are url parameters, only put the range of dates between start and end time
+            start = request.args.get("start_time")
+            end = request.args.get("end_time")
+            dates = Calendar.query.filter(Calendar.date.between(start, end)).all()
+        else:  # Else, query all events
+            dates = Calendar.query.all()
         list_of_dates = list()
-        info = dict()
         for date in dates:
-            info["id"] = date.id
-            info["event"] = date.event
-            info["date"] = str(date.date)
-            list_of_dates.append(info)
-            info = dict()
+            list_of_dates.append({"id": date.id, "event": date.event, "date": str(date.date)})
         return list_of_dates
 
-    def post(self):
-        args = parser.parse_args()
-        args["date"] = str(args["date"].date())
-        args["message"] = "The event has been added!"
-        db_date = Calendar(event=args["event"], date=datetime.strptime(args["date"], "%Y-%m-%d"))
+    def post(self):  # You can only add events on this url
+        args = parser.parse_args()  # First, make sure everything is formatted properly
+        args["message"] = "The event has been added!"  # If it is, then this success message will pop up
+        db_date = Calendar(event=args["event"], date=args["date"])  # Add it to database
         db.session.add(db_date)
         db.session.commit()
-        return args
+        return {"message": args["message"], "event": args["event"], "date": str(args["date"].date())}
 
 
-class apiTodayResource(Resource):
+class apiTodayResource(Resource):  # This is for event/today
     def get(self):
-        dates = Calendar.query.all()
+        dates = Calendar.query.all()  # Query all events in database
         list_of_dates = list()
-        info = dict()
         for date in dates:
-            if str(date.date) == str(datetime.today().date()):
-                info["id"] = date.id
-                info["event"] = date.event
-                info["date"] = str(date.date)
-                list_of_dates.append(info)
-            info = dict()
+            if str(date.date) == str(datetime.today().date()):  # If there is an event today, display it!
+                list_of_dates.append({"id": date.id, "event": date.event, "date": str(date.date)})
         return list_of_dates
 
 
-db.create_all()
+class EventByID(Resource):  # This is for event/# where # is the ID of the event (GET /event shows all ids)
+    def get(self, event_id):  # You can retrieve events by ID
+        dates = Calendar.query.filter_by(id=event_id).first()
+        if dates is None:
+            abort(404, "The event doesn't exist!")
+        return {"id": dates.id, "event": dates.event, "date": str(dates.date)}
 
+    def delete(self, event_id):  # You can delete events
+        dates = Calendar.query.filter_by(id=event_id).first()
+        if dates is None:
+            abort(404, "The event doesn't exist!")
+        db.session.delete(dates)  # Delete from DB
+        db.session.commit()
+        return {"message": "The event has been deleted!"}
+
+
+# These add to the API so that we can do proper HTTP commands
 api.add_resource(apiResource, '/event')
 api.add_resource(apiTodayResource, '/event/today')
+api.add_resource(EventByID, '/event/<int:event_id>')  # <int:event_id> = any integer thrown into event_id
 
+# This is here to run the URL on your own computer
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         arg_host, arg_port = sys.argv[1].split(':')
